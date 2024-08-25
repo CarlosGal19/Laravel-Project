14f300ea0387805ff9158768e8d4aead86f4d50b
Carlos Galindo <utm22030587@utma.edu.mx>
2024-08-25 16:42:55 -0600

    feat: create relationships between models

---
+++ database/migrations/2024_08_25_223113_create_idea_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('idea_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idea_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('idea_id')->references('id')->on('ideas')->cascadeOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('idea_user');
+    }
+};
